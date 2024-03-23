0838d89072928ba5f4e2153fe7443f964b43d392
Pasan Hewavitharana <115849043+pasan2002@users.noreply.github.com>
2024-03-24 00:24:19 +0530

    Update public.js

--- server/routes/public.js
+++ server/routes/public.js
@@ -9,8 +9,12 @@ const {
     getAllSkills,
 } = require("../controllers/skills")
 
+const{
+    getAllHistory,
+}=require("../controllers/history")
 
 router.route("/").get(getAllProjects)
 router.route("/publicskills").get(getAllSkills)
+router.route("/publichistory").get(getAllHistory)
 
 module.exports = router
